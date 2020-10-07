6e0b51eeca0c813bbce18df6c7bc2ad8530cdd05
webyilmaz-services <kylmz2009@gmail.com>
2020-10-07 16:11:43 +0300

    v1.0

---
+++ MyCV/MyCV/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.Mvc;
+using MyCV.Models.EntityFramework;
+using MyCV.Models.Class;
+
+namespace MyCV.Controllers
+{
+    public class DefaultController : Controller
+    {
+        CVDBEntities1 db = new CVDBEntities1();
+        // GET: Default
+        public ActionResult Index()
+        {
+            
+            MyCVClass cvClass = new MyCVClass();
+
+            cvClass.dbMe = db.tblMes.ToList();
+            cvClass.dbSite = db.tblSites.ToList();
+            cvClass.dbSkill = db.tblSkills.ToList();
+
+
+            return View(cvClass);
+            
+        }
+    }
+}
---
+++ MyCV/MyCV/Models/Class/MyCVClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyCV.Models.EntityFramework;
+
+namespace MyCV.Models.Class
+{
+    public class MyCVClass
+    {
+        public IEnumerable<tblMe> dbMe { get; set; }
+        public IEnumerable<tblSite> dbSite { get; set; }
+        public IEnumerable<tblSkill> dbSkill { get; set; }
+    }
+}
